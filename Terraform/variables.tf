07b228c87f7765568f992bbb864db3d496cc10d6
HuuPhat125 <phatd142@gmail.com>
2025-04-28 21:56:48 +0700

    feat: create empty vpc

---
+++ Terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-southeast-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+}
