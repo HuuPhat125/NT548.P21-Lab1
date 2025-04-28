8a9f805225d84e68ba45a27c9598ac0abe7212fe
HuuPhat125 <phatd142@gmail.com>
2025-04-28 23:48:15 +0700

    feat: create itg

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -7,3 +7,9 @@ module "vpc" {
   cidr_block = var.vpc_cidr
   name       = "my-vpc"
 }
+
+module "internet_gateway" {
+  source = "./modules/internet-gateway"
+  vpc_id = module.vpc.vpc_id
+  name   = "my-igw"
+}
