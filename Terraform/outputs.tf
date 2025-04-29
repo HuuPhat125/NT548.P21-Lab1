ac6aa0bd78dd34d96230429f75b583108e0f09fe
chauthevi2004 <22521653@gm.uit.edu.vn>
2025-04-29 20:33:33 +0700

    fix: ec2 output

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -2,12 +2,20 @@ output "vpc_id" {
   value = module.vpc.vpc_id
 }
 
-output "public_ec2_public_ip" {
+output "public_ec2_id" {
   value = module.ec2_public.instance_id
+}
+
+output "public_ec2_ip" {
+  value = module.ec2_public.public_ip
 }
 
 output "private_ec2_id" {
   value = module.ec2_private.instance_id
+}
+
+output "private_ec2_ip" {
+  value = module.ec2_private.private_ip
 }
 
 output "nat_gateway_id" {
