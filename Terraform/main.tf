b6317e1ca444e2bcdbc588db57ef637e9c1656ac
HuuPhat125 <phatd142@gmail.com>
2025-04-29 08:59:14 +0700

    feat: add subnet

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -13,3 +13,21 @@ module "internet_gateway" {
   vpc_id = module.vpc.vpc_id
   name   = "my-igw"
 }
+
+module "public_subnet" {
+  source          = "./modules/subnet"
+  vpc_id          = module.vpc.vpc_id
+  cidr_block      = var.public_subnet_cidr
+  az              = var.az_1
+  map_public_ip   = true
+  name            = "public-subnet"
+}
+
+module "private_subnet" {
+  source          = "./modules/subnet"
+  vpc_id          = module.vpc.vpc_id
+  cidr_block      = var.private_subnet_cidr
+  az              = var.az_1
+  map_public_ip   = false
+  name            = "private-subnet"
+}
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -8,3 +8,18 @@ variable "vpc_cidr" {
   description = "CIDR block for VPC"
   type        = string
 }
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for public subnet"
+  type        = string
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for private subnet"
+  type        = string
+}
+
+variable "az_1" {
+  description = "Availability Zone"
+  type        = string
+}
